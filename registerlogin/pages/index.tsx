89549b1349b29c922ce0c44482dad6a046eb1583
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-09-10 15:50:42 -0300

    add hooks

--- registerlogin/pages/index.tsx
+++ registerlogin/pages/index.tsx
@@ -2,34 +2,32 @@ import Head from "next/head"
 import "../app/globals.css"
 import Link from "next/link"
 import Swal from "sweetalert2"
+import { useState } from "react";
 
 export default function Page() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
 
-  const register = () => {
-    const inputNome: any = document.getElementById('nome')
-    const inputEmail: any = document.getElementById('email')
-    const inputSenha: any = document.getElementById('senha')
+  const register = () => {    
 
-    const dados = {
-      nome: inputNome.value,
-      email: inputEmail.value,
-      senha: inputSenha.value
-    };
+      if (name && email && password) {
+        const data ={
+          name,
+          email,
+          password
+        }
 
-    if (dados.nome && dados.email && dados.senha) {
-
-      localStorage.setItem("dados", JSON.stringify(dados))
+      localStorage.setItem("dados", JSON.stringify(data))
 
       Swal.fire(
         'Conta registrada com sucesso!',
         '',
         'success'
       )
-
-      inputNome.value = ""
-      inputEmail.value = ""
-      inputSenha.value = ""
-      
+      setName('');
+      setEmail('');
+      setPassword('')
     }
     else {
       Swal.fire(
@@ -37,8 +35,10 @@ export default function Page() {
         '',
         'error'
       )
+
     }
   }
+ 
 
   return (
     <>     
@@ -47,9 +47,9 @@ export default function Page() {
         </Head>
         <section className="font-mono font-semibold flex flex-col w-80 mx-auto mt-20 bg-sky-700 p-3 rounded-xl">
           <h1 className="h-10 flex items-center justify-center text-7x1 text-white underline font-bold">Registre sua conta!</h1>
-          <input className="h-10 mt-20 rounded-xl px-2" id="nome" type="text" placeholder="Insira o seu nome" />
-          <input className="h-10 mt-5 rounded-xl px-2" id="email" type="text" placeholder="Email" />
-          <input className="h-10 mt-5 rounded-xl px-2" id="senha" type="text" placeholder="Senha" />
+          <input className="h-10 mt-20 rounded-xl px-2" type="text" placeholder="Insira o seu nome" onChange={(e) => setName(e.target.value)} />
+          <input className="h-10 mt-5 rounded-xl px-2" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+          <input className="h-10 mt-5 rounded-xl px-2" type="text" placeholder="Senha" onChange={(e) => setPassword(e.target.value)}/>
           <button className="h-10 mt-10 text-xl bg-sky-400 rounded-xl p-2 font-bold" type="submit" onClick={register}>Registrar!</button>
           <Link className="flex items-center justify-center mx-auto mt-20 h-10 w-48 bg-sky-50 text-xl rounded-xl font-bold" href="/posts/login">Fazer login!</Link>
         </section>
