c0bf6ad80913e9a17ddd9bbd2efbb5dc32abcd78
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-09-12 09:51:39 -0300

    test

--- registerlogin/components/Inputs.tsx
+++ registerlogin/components/Inputs.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Input from '@mui/joy/Input';
 
-const Inputs = () => {
+export default function Inputs() {
   return (
-    <div>Inputs</div>
-  )
+    <Box
+      sx={{
+        py: 2,
+        display: 'grid',
+        gap: 2,
+        alignItems: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+      <Input placeholder="Type in here…" variant="outlined" color="primary" />
+      <Input placeholder="Type in here…" variant="outlined" color="neutral" />
+      <Input placeholder="Type in here…" variant="outlined" color="danger" />
+      <Input placeholder="Type in here…" variant="outlined" color="success" />
+      <Input placeholder="Type in here…" variant="outlined" color="warning" />
+    </Box>
+  );
 }
-
-export default Inputs
